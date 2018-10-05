7bacff7e3586a1995d54675d239735a01e599ed4
Ian Haken <ihaken@netflix.com>
2018-10-05 11:42:02 -0700

    Add some initial tool scripts.

---
+++ tools/ctl_parser_structures.py
@@ -0,0 +1,25 @@
+from construct import Struct, Byte, Int16ub, Int64ub, Enum, Bytes, Int24ub, this, GreedyBytes, GreedyRange, Terminated, Embedded
+
+MerkleTreeHeader = Struct(
+    "Version"         / Byte,
+    "MerkleLeafType"  / Byte,
+    "Timestamp"       / Int64ub,
+    "LogEntryType"    / Enum(Int16ub, X509LogEntryType=0, PrecertLogEntryType=1),
+    "Entry"           / GreedyBytes
+)
+
+Certificate = Struct(
+    "Length" / Int24ub,
+    "CertData" / Bytes(this.Length)
+)
+
+CertificateChain = Struct(
+    "ChainLength" / Int24ub,
+    "Chain" / GreedyRange(Certificate),
+)
+
+PreCertEntry = Struct(
+    "LeafCert" / Certificate,
+    Embedded(CertificateChain),
+    Terminated
+)
